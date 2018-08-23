e259f76ce7bf40af9cba3a40abcf8cd15aa46917
Cheng Li <cli99@illinois.edu>
2018-08-23 04:15:16 +0000

    add alexnet result

--- inference/alexnet_infer.py
+++ inference/alexnet_infer.py
@@ -52,8 +52,8 @@ with tf.Graph().as_default() as graph:
                       run_metadata=run_metadata, feed_dict=feed_dict_testing)
 
     ProfileOptionBuilder = tf.profiler.ProfileOptionBuilder
-    opts = (ProfileOptionBuilder(ProfileOptionBuilder.time_and_memory().with_step(0)
-                                 .with_timeline_output("alexnet_profile.json")).build())
+    opts = (ProfileOptionBuilder(ProfileOptionBuilder.time_and_memory()).with_step(0)
+                                 .with_timeline_output("alexnet_profile.json").build())
 
     tf.profiler.profile(
         graph,
