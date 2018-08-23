defcd0b53759c6cf252193215a1589d5598babb1
Ubuntu <ubuntu@ip-172-31-26-89.ec2.internal>
2018-08-23 17:08:35 +0000

    update

--- utils/query.py
+++ utils/query.py
@@ -1,4 +1,7 @@
+import tensorflow as tf
 from tensorflow.python.client import device_lib
+
+print("tensorflow version = ", tf.__version__ )
 
 def get_available_gpus():
     local_device_protos = device_lib.list_local_devices()
